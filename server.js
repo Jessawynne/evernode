27ffeff0f5093cf7538cca61ed84f19c80e3ebb2
Jessawynne <jessawynne@gmail.com>
2016-02-18 11:24:51 -0600

    show new note in browser

--- server.js
+++ server.js
@@ -24,15 +24,25 @@ app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
+
 app.get('/notes/new', (req, res) => {
   res.render('new-note');
+});
+
+//routing order MATTERS when using a routing param aka :id
+app.get('/notes/:id', (req, res) => {
+  Note.findById(req.params.id, (err, note) => {
+    if (err) throw err;
+
+    res.render('show-note', {note: note});
+  });
 });
 
 app.post('/notes', (req, res) => {
   Note.create(req.body, (err, note) => {
     if (err) throw err;
-    console.log(note);
-    res.redirect('/');    
+
+    res.redirect(`/notes/${note._id}`);
   });
 });
 
@@ -43,3 +53,5 @@ mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
     console.log(`Evernode server running on port: ${port}`);
   });
 });
+
+
