f30fd9659226a767ae067b7a87553958b97bb446
Jessawynne <jessawynne@gmail.com>
2016-02-18 10:53:14 -0600

    save new note to db

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Note = mongoose.model('Notes',  mongoose.Schema({
+    title: String,
+    text: String
+}));
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,10 +29,17 @@ app.get('/notes/new', (req, res) => {
 });
 
 app.post('/notes', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log(note);
+    res.redirect('/');    
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Evernode server running on port: ${port}`);
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
 });
