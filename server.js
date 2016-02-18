fad0be2c0fb363d9ac3711017851d77e9a9e35a7
Jessawynne <jessawynne@gmail.com>
2016-02-18 11:38:30 -0600

    refactored note routes into file

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const noteRoute = require('./routes/notes');
 const app = express();
 const port = process.env.PORT || 3000;
-
-const Note = require('./models/note');
 
 //all middlewares come before all routes
 app.set('view engine', 'jade');
@@ -20,28 +19,7 @@ app.use(bodyParser.urlencoded({
 app.get('/', (req, res) => {
   res.send('Server Running');
 });
-
-
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-
-//routing order MATTERS when using a routing param aka :id
-app.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {note: note});
-  });
-});
-
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-
-    res.redirect(`/notes/${note._id}`);
-  });
-});
+app.use(noteRoute);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
